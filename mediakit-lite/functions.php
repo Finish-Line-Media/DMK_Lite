e791c8282b1591611da8f944ecbe3e130709aeab
Nathaniel Holzmann <nathanielholzmann@Innovation-Station.local>
2025-06-22 11:17:06 -0700

    Fix version number to 1.5.1

--- mediakit-lite/functions.php
+++ mediakit-lite/functions.php
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Define Constants
  */
-define( 'MKP_THEME_VERSION', 'patch' );
+define( 'MKP_THEME_VERSION', '1.5.1' );
 define( 'MKP_THEME_DIR', get_template_directory() );
 define( 'MKP_THEME_URI', get_template_directory_uri() );
 
