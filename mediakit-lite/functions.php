cf2ccf61a83f82173e5b27e2cf5b57fe3ca8b2d1
Nathaniel Holzmann <nathanielholzmann@Innovation-Station.local>
2025-06-24 21:25:04 -0500

    Bump version to 1.7.22

--- mediakit-lite/functions.php
+++ mediakit-lite/functions.php
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Define Constants
  */
-define( 'MKP_THEME_VERSION', '1.7.21' );
+define( 'MKP_THEME_VERSION', '1.7.22' );
 define( 'MKP_THEME_DIR', get_template_directory() );
 define( 'MKP_THEME_URI', get_template_directory_uri() );
 
